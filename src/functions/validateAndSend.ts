4623da6040b569764e540b0ed7a4a537fbfa1be1
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-09-20 20:44:25 +0300

    Cleaning up

--- src/functions/validateAndSend.ts
+++ src/functions/validateAndSend.ts
@@ -1,13 +1,10 @@
-import { Context, Callback, Handler, APIGatewayProxyResult } from 'aws-lambda';
+import { Context, Callback, Handler } from 'aws-lambda';
 import { sendMessage } from '../lib/Twilio';
 import { validateRecaptcha } from '../lib/Recaptcha';
 import { IEvent } from '../types';
 import * as Config from '../lib/Config';
 
 export const validateAndSend: Handler = async (event: IEvent, context: Context, callback: Callback) => {
-  // Required in responses for CORS support to work
-  const headers = {'Access-Control-Allow-Origin': '*'};
-
   if (Config.get('NODE_ENV') === 'test') {
     event.body = {
       "captcha": "REPLACE_WITH_VALID_CAPTCHA_FROM_WEBSITE",
